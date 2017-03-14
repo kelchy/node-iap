32ad5ec15d862ee0be5fc30e251e5309fc437938
Kelvin Chua <kelchy@gmail.com>
2017-03-14 06:47:04 +0000

    add support for voided transactions

--- index.js
+++ index.js
@@ -31,3 +31,31 @@ exports.verifyPayment = function (platform, payment, cb) {
 		cb(null, result);
 	});
 };
+
+exports.voided = function (platform, payment, cb) {
+	function syncError(error) {
+		process.nextTick(function () {
+			cb(error);
+		});
+	}
+
+	if (platform != "google") {
+		return syncError(new Error('Not supported on platform'));
+	}
+
+	var engine = platforms[platform];
+
+	if (!engine) {
+		return syncError(new Error('Platform ' + platform + ' not recognized'));
+	}
+
+	engine.voided(payment, function (error, result) {
+		if (error) {
+			return cb(error);
+		}
+
+		result.platform = platform;
+
+		cb(null, result);
+	});
+};
